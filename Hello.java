5d1ec9892f740788f3cbc6c3ed51b6b7bb76d06f
chutima42 <tik_985@hotmail.com>
2016-03-30 15:42:30 +0700

    Update Hello.java
    แก้ไฟล์

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String args[]) {
-    String myname = "sek ka tik";
+    String myname = "chutima";
     System.out.println("My name is" + myname);
   }
 }
